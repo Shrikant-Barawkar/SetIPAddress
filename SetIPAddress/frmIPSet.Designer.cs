a392e6a2c2aaf0d9ef5ef91c58f6ee6a6aa4b884
admin <admin@DESKTOP-OFUHSAK>
2025-07-11 14:02:06 +0530

    Add project files.

---
+++ SetIPAddress/frmIPSet.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace SetIPAddress
+{
+    partial class frmIPSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbPTselection = new System.Windows.Forms.ComboBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.lblHostName = new System.Windows.Forms.Label();
+            this.textBoxInterfaceName = new System.Windows.Forms.TextBox();
+            this.textBoxNewIP = new System.Windows.Forms.TextBox();
+            this.textBoxSubnetMask = new System.Windows.Forms.TextBox();
+            this.textBoxGateway = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textBoxHostName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbInterfaceName = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmbPTselection
+            // 
+            this.cmbPTselection.FormattingEnabled = true;
+            this.cmbPTselection.Location = new System.Drawing.Point(106, 57);
+            this.cmbPTselection.Name = "cmbPTselection";
+            this.cmbPTselection.Size = new System.Drawing.Size(121, 21);
+            this.cmbPTselection.TabIndex = 0;
+            this.cmbPTselection.SelectedValueChanged += new System.EventHandler(this.cmbPTselection_SelectedValueChanged);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(75, 282);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(103, 39);
+            this.btnLoad.TabIndex = 14;
+            this.btnLoad.Text = "LOAD";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // lblHostName
+            // 
+            this.lblHostName.AutoSize = true;
+            this.lblHostName.Location = new System.Drawing.Point(31, 60);
+            this.lblHostName.Name = "lblHostName";
+            this.lblHostName.Size = new System.Drawing.Size(69, 13);
+            this.lblHostName.TabIndex = 20;
+            this.lblHostName.Text = "Host Name : ";
+            // 
+            // textBoxInterfaceName
+            // 
+            this.textBoxInterfaceName.Location = new System.Drawing.Point(150, 327);
+            this.textBoxInterfaceName.Name = "textBoxInterfaceName";
+            this.textBoxInterfaceName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInterfaceName.TabIndex = 25;
+            this.textBoxInterfaceName.Visible = false;
+            // 
+            // textBoxNewIP
+            // 
+            this.textBoxNewIP.Location = new System.Drawing.Point(121, 140);
+            this.textBoxNewIP.Name = "textBoxNewIP";
+            this.textBoxNewIP.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNewIP.TabIndex = 26;
+            // 
+            // textBoxSubnetMask
+            // 
+            this.textBoxSubnetMask.Location = new System.Drawing.Point(121, 170);
+            this.textBoxSubnetMask.Name = "textBoxSubnetMask";
+            this.textBoxSubnetMask.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSubnetMask.TabIndex = 27;
+            // 
+            // textBoxGateway
+            // 
+            this.textBoxGateway.Location = new System.Drawing.Point(121, 196);
+            this.textBoxGateway.Name = "textBoxGateway";
+            this.textBoxGateway.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGateway.TabIndex = 28;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Interface Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Subnet Mask";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Gateway";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(86, 243);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 29;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // textBoxHostName
+            // 
+            this.textBoxHostName.Location = new System.Drawing.Point(121, 88);
+            this.textBoxHostName.Name = "textBoxHostName";
+            this.textBoxHostName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHostName.TabIndex = 31;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Host Name";
+            // 
+            // cmbInterfaceName
+            // 
+            this.cmbInterfaceName.FormattingEnabled = true;
+            this.cmbInterfaceName.Location = new System.Drawing.Point(120, 114);
+            this.cmbInterfaceName.Name = "cmbInterfaceName";
+            this.cmbInterfaceName.Size = new System.Drawing.Size(101, 21);
+            this.cmbInterfaceName.TabIndex = 32;
+            // 
+            // frmIPSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 350);
+            this.Controls.Add(this.cmbInterfaceName);
+            this.Controls.Add(this.textBoxHostName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBoxGateway);
+            this.Controls.Add(this.textBoxSubnetMask);
+            this.Controls.Add(this.textBoxNewIP);
+            this.Controls.Add(this.textBoxInterfaceName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHostName);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.cmbPTselection);
+            this.Name = "frmIPSet";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Set IP Address";
+            this.Load += new System.EventHandler(this.frmIPSet_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPTselection;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label lblHostName;
+        private System.Windows.Forms.TextBox textBoxInterfaceName;
+        private System.Windows.Forms.TextBox textBoxNewIP;
+        private System.Windows.Forms.TextBox textBoxSubnetMask;
+        private System.Windows.Forms.TextBox textBoxGateway;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBoxHostName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbInterfaceName;
+    }
+}
+
---
+++ SetIPAddress/frmIPSet.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Management;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace SetIPAddress
+{
+    public partial class frmIPSet : Form
+    {
+
+        public frmIPSet()
+        {
+            InitializeComponent();
+        }
+
+
+        private string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;DATA SOURCE=" + Application.StartupPath + "\\SetIP.mdb;";
+
+        string interfaceName;  // Change to your actual interface name
+        string newIP;    // New IP Address
+        string subnetMask;// Subnet Mask
+        string gateway;   // Default Gateway
+
+        public OleDbConnection DBConnection;
+        public OleDbCommand DBCommand;
+        public OleDbDataReader DBReader;
+        private void LoadComboBoxData()
+        {
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                cmbPTselection.Items.Clear();
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM SetIP", conn);
+                OleDbDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    cmbPTselection.Items.Add(reader["HostName"].ToString());
+                }
+                cmbPTselection.SelectedValue = null;
+            }
+
+            btnSave.Visible = false;
+
+
+            //Adding availabel interface names in combox selection of interface name.
+            cmbInterfaceName.Items.Clear(); // Clear existing items
+
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // Add all interface names
+                cmbInterfaceName.Items.Add(nic.Name);
+            }
+
+            // Optionally select the first item
+            //if (cmbInterfaceName.Items.Count > 0)
+            //    cmbInterfaceName.SelectedIndex = 0;
+        }
+        private void LoadSelectedHostDetails(string hostname)
+        {
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM SetIP WHERE HostName = @hostname", conn);
+                cmd.Parameters.AddWithValue("@hostname", hostname);
+
+                OleDbDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    textBoxHostName.Text = reader["HostName"].ToString();
+                    //textBoxInterfaceName.Text = reader["InterfaceName"].ToString();
+                    cmbInterfaceName.Text = reader["InterfaceName"].ToString();
+                    textBoxNewIP.Text = reader["IPaddress"].ToString();
+                    textBoxGateway.Text = reader["DefaultGateway"].ToString();
+                    textBoxSubnetMask.Text = reader["SubnetMask"].ToString();
+
+                    // Disable text boxes to prevent editing
+                    EnableTextBoxes(false);
+                }
+            }
+        }
+        private void ClearTextBoxes()
+        {
+            //textBoxInterfaceName.Clear();
+            cmbInterfaceName.SelectedIndex=-1;
+            textBoxNewIP.Clear();
+            textBoxGateway.Clear();
+            textBoxSubnetMask.Clear();
+        }
+
+        private void EnableTextBoxes(bool newStatus)
+        {
+            //textBoxInterfaceName.Enabled = newStatus;
+            cmbInterfaceName.Enabled = newStatus;
+            textBoxNewIP.Enabled = newStatus;
+            textBoxGateway.Enabled = newStatus;
+            textBoxSubnetMask.Enabled = newStatus;
+        }
+
+        // Function to change IP address using netsh
+        static void ChangeIPAddress(string interfaceName, string newIP, string subnetMask, string gateway)
+        {
+            //string command = string.Format("netsh interface ip set address \\{0}\\static {1} {2} {3}", interfaceName, newIP, subnetMask, gateway);
+            string command = $"netsh interface ip set address {interfaceName} static {newIP} {subnetMask} {gateway}";
+
+            //string command = $"netsh interface ip set address \"{interfaceName}\" static {newIP} {subnetMask} {gateway}";
+            ExecuteCommand(command);
+        }
+
+        // Function to verify the IP address change
+        static void VerifyIPAddress(string expectedIP)
+        {
+            string[] ipSplit = expectedIP.Split('.');
+            expectedIP = string.Format("{0}.{1}.{2}.{3}", int.Parse(ipSplit[0]), int.Parse(ipSplit[1]), int.Parse(ipSplit[2]), int.Parse(ipSplit[3]));
+            string command = "ipconfig";
+            string output = ExecuteCommand(command);
+            Thread.Sleep(5000);
+            MessageBox.Show(expectedIP + "\n\n" + output);
+            if (output.Contains(expectedIP))
+            {
+                MessageBox.Show(string.Format("IP Address successfully changed to: {0}", expectedIP));
+            }
+            else
+            {
+                MessageBox.Show("Failed to change the IP address.");
+            }
+        }
+
+        // Execute a command and return the output
+        static string ExecuteCommand(string command)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/c " + command)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(processStartInfo))
+            using (System.IO.StreamReader reader = process.StandardOutput)
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private void frmIPSet_Load(object sender, EventArgs e)
+        {
+            // Check if the application is running with administrator privileges
+            if (!IsRunningAsAdministrator())
+            {
+                // Restart the application with administrator privileges
+                RestartWithAdminRights();
+                MessageBox.Show("Not with administrator privileges.");
+            }
+            else
+            {
+                // Your code that requires elevated privileges
+                MessageBox.Show("Running with administrator privileges.");
+            }
+
+            LoadComboBoxData();
+        }
+
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //setIP(mtxtIP.Text, mtxtSubnet.Text);
+            //ChangeIPAddress(textBoxInterfaceName.Text, textBoxNewIP.Text, textBoxSubnetMask.Text, textBoxGateway.Text);
+            ChangeIPAddress(cmbInterfaceName.Text, textBoxNewIP.Text, textBoxSubnetMask.Text, textBoxGateway.Text);
+            Thread.Sleep(5000);
+            //        // Verify if the IP address was changed successfully
+            VerifyIPAddress(textBoxNewIP.Text);
+        }
+
+        private void cmbPTselection_SelectedValueChanged(object sender, EventArgs e)
+        {
+            //if (cmbPTselection.Text == "TRIO")
+            //{
+            //    // Call the function to change the IP address
+            //    interfaceName = "Ethernet";  // Change to your actual interface name
+            //    newIP = "192.168.0.250";    // New IP Address
+            //    subnetMask = "255.255.255.0"; // Subnet Mask
+            //    gateway = "192.168.0.10";   // Default Gateway
+            //}
+            //else if (cmbPTselection.Text == "PMAC")
+            //{
+            //    // Call the function to change the IP address
+            //    interfaceName = "Ethernet";  // Change to your actual interface name
+            //    newIP = "192.168.1.250";    // New IP Address
+            //    subnetMask = "255.255.255.0"; // Subnet Mask
+            //    gateway = "192.168.0.10";   // Default Gateway
+            //}
+            //else if (cmbPTselection.Text == "Work")
+            //{
+            //    // Call the function to change the IP address
+            //    interfaceName = "Ethernet";  // Change to your actual interface name
+            //    newIP = "10.32.0.178";    // New IP Address
+            //    subnetMask = "255.255.255.0"; // Subnet Mask
+            //    gateway = "10.32.0.10";   // Default Gateway
+            //}
+            //else if (cmbPTselection.Text == "Personal")
+            //{
+            //    // Call the function to change the IP address
+            //    interfaceName = "Ethernet";  // Change to your actual interface name
+            //    newIP = "192.148.0.200";    // New IP Address
+            //    subnetMask = "255.255.255.0"; // Subnet Mask
+            //    gateway = "192.148.0.100";   // Default Gateway
+            //}
+            //else if (cmbPTselection.Text == "---NEW---")
+            //{
+
+            //}
+            if (cmbPTselection.SelectedItem.ToString() == "--New--")
+            {
+                // Clear text boxes for new entry
+                textBoxHostName.Clear();
+                //textBoxInterfaceName.Clear();
+                cmbInterfaceName.SelectedIndex = -1;
+                textBoxNewIP.Clear();
+                textBoxGateway.Clear();
+                textBoxSubnetMask.Clear();
+
+                // Enable text boxes for input
+                EnableTextBoxes(true);
+                btnSave.Visible = true;
+            }
+            else
+            {
+                // Load selected host details if not adding new
+                LoadSelectedHostDetails(cmbPTselection.SelectedItem.ToString());
+                btnSave.Visible = false;
+            }
+
+        }
+
+
+        // Check if the application is running with administrator privileges
+        static bool IsRunningAsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        // Restart the application with administrator privileges
+        static void RestartWithAdminRights()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.Verb = "runas";  // This will trigger the UAC prompt
+            startInfo.FileName = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            Process.Start(startInfo);
+            Environment.Exit(0);  // Exit the current process
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (cmbPTselection.SelectedItem.ToString() == "--NEW--")
+            {
+                // Save new host entry
+                using (OleDbConnection conn = new OleDbConnection(connectionString))
+                {
+                    conn.Open();
+                    OleDbCommand cmd = new OleDbCommand("INSERT INTO SetIP (HostName, InterfaceName, IPaddress, DefaultGateway, SubnetMask) VALUES (@hostname, @interfaceName, @ip, @gateway, @subnet)", conn);
+                    cmd.Parameters.AddWithValue("@hostname", textBoxHostName.Text); // Use the selected value as hostname
+                    //cmd.Parameters.AddWithValue("@interfaceName", textBoxInterfaceName.Text);
+                    cmd.Parameters.AddWithValue("@interfaceName", cmbInterfaceName.Text);
+                    cmd.Parameters.AddWithValue("@ip", textBoxNewIP.Text);
+                    cmd.Parameters.AddWithValue("@gateway", textBoxGateway.Text);
+                    cmd.Parameters.AddWithValue("@subnet", textBoxSubnetMask.Text);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("New entry saved successfully.");
+
+                    // Reload combo box data
+                    LoadComboBoxData();
+                }
+            }
+        }
+    }
+}
